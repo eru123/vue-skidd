64388e6fbfcc15e4cc8d883156eeadf38cfd3cb3
eru123 <yeoligoakino@gmail.com>
2021-06-02 19:42:40 +0800

    added: fire helper

--- src/plugins/fire.js
+++ src/plugins/fire.js
@@ -18,19 +18,77 @@ const init = function (config) {
   };
 };
 
+const helper = (firebaseInstance) => {
+  const arrayUnion = firebase.firestore.FieldValue.arrayUnion;
+  const arrayRemove = firebase.firestore.FieldValue.arrayRemove;
+
+  const login = async (email, pass) => {
+    return await firebaseInstance.auth.signInWithEmailAndPassword(email, pass);
+  };
+
+  const register = async (email, pass) => {
+    return await firebaseInstance.auth.createUserWithEmailAndPassword(
+      email,
+      pass
+    );
+  };
+
+  const set = async (collection, doc, data, overwrite = false) => {
+    return await firebaseInstance.firestore
+      .collection(collection)
+      .doc(doc)
+      .set(data, overwrite ? { merge: true } : {});
+  };
+
+  const sendEmailVerification = async () => {
+    return await firebaseInstance.auth.currentUser.sendEmailVerification();
+  };
+
+  const auth = (callback = null) => {
+    return firebaseInstance.auth.onAuthStateChanged(callback);
+  };
+
+  const user = () => {
+    return firebaseInstance.auth.currentUser;
+  };
+
+  const mediaUrl = async (src) => {
+    return await firebaseInstance.storage.ref(src).getDownloadURL();
+  };
+
+  return {
+    user,
+    auth,
+    login,
+    register,
+    sendEmailVerification,
+    set,
+    arrayUnion,
+    arrayRemove,
+    mediaUrl,
+  };
+};
+
 export default {
-  // Your web app's Firebase configuration
-  // const config = {
-  //   apiKey,
-  //   authDomain,
-  //   databaseURL,
-  //   projectId,
-  //   storageBucket,
-  //   messagingSenderId,
-  //   appId,
-  //   measurementId,
-  // }
+  /*
+    Your web app's Firebase configuration
+    const config = {
+      apiKey,
+      authDomain,
+      databaseURL,
+      projectId,
+      storageBucket,
+      messagingSenderId,
+      appId,
+      measurementId,
+    }
+  */
   install: function (Vue, config) {
-    Vue.prototype.$fire = init(config);
+    const firebaseInstance = init(config);
+
+    Vue.prototype.$fire = {
+      ...firebaseInstance,
+      helper: helper(firebaseInstance),
+    };
   },
 };
